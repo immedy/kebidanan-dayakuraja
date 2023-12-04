3202fba3227ff21e215dca103bbb70d560798639
Rahmat Sholeh <38641053+dvg32@users.noreply.github.com>
2023-12-04 13:55:49 +0800

    create Rujukan model --all
    create Rujukan model --all

---
+++ app/Http/Controllers/RujukanController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Rujukan;
+use App\Http\Requests\StoreRujukanRequest;
+use App\Http\Requests\UpdateRujukanRequest;
+
+class RujukanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRujukanRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Rujukan $rujukan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Rujukan $rujukan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRujukanRequest $request, Rujukan $rujukan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Rujukan $rujukan)
+    {
+        //
+    }
+}
